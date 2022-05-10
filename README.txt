25db9db302cf55e0253cd33f2a4b8b38ee68cf96
McIntyre-Colin <colinhmcintyre@gmail.com>
2022-05-10 16:43:50 -0400

    Please work

---
+++ README.txt
@@ -0,0 +1,1 @@
+Making sure git environment is working
---
+++ urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+
+import views
+
+# In this example, we've separated out the views.py into a new file
+urlpatterns = [
+    path('', views.index),
+    path('state-data', views.state_data),
+    path('comparison/', views.comparison)
+]
+
+# Boilerplate to include static files
+from django.conf import settings
+from django.conf.urls.static import static
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
